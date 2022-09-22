300e0537f657837330a117e9ade33664b828a18c
shopnilashik <shopnil.ashik@gmail.com>
2022-09-22 18:56:04 +0600

    delete oparation, calculation

--- app/dahill/webapp/controller/InvoiceController/CreateInvoice.controller.js
+++ app/dahill/webapp/controller/InvoiceController/CreateInvoice.controller.js
@@ -12,16 +12,31 @@ sap.ui.define([
 
         return Controller.extend("dahill.dahill.controller.InvoiceController.CreateInvoice", {
             onInit: function () {
-              
                 this.count = 1;
                 var oJsonModel = new JSONModel({
                     Items:[
-                       
+                       {
+                        id: 1,
+                        description: "shopnil",
+                        amount: "1000",
+                       },
+                       {
+                        id: 2,
+                        description: "Ashik",
+                        amount: "2000",
+                       }
                     ]
                 })
+                var oTotalCounterModel = new JSONModel({
+                   total:0
+                })
                 this.getView().setModel(oJsonModel, "oItemData");
+                this.getView().setModel(oTotalCounterModel, "oTotalCounterModel");
                 this._description = this.byId("Invoice_description_textarea");
                 this._amount = this.byId("input_amount");
+                this._total = 0;
+                this._totalCalculation();
+
             },
             onCustomerHelper:function(){
                 var oView = this.getView();
@@ -48,8 +63,6 @@ sap.ui.define([
                 var oBinding = oEvent.getSource().getBinding("items");
                 oBinding.filter([oFilter]);
             },	
-           
-
             handleValueHelpClose: function (oEvent) {
                 var oSelectedItem = oEvent.getParameter("selectedItem"),
                 oInput = this.byId("CustomerInput");
@@ -57,7 +70,6 @@ sap.ui.define([
                     oInput.resetProperty("value");
                     return;
                 }
-                
                 oInput.setValue(oSelectedItem.getCells()[0].getTitle());
                 this.byId("Inoice_input_address").setValue(oSelectedItem.getCells()[0].getTitle());
                 this.byId("Inoice_input_address").setValue(oSelectedItem.getCells()[1].getText());
@@ -67,11 +79,9 @@ sap.ui.define([
             onSavePressed:function(){
                 var test = this.byId("invoice_Date").getValue();
                 console.log(test);
-               
             },
             onAddItem:function(){ 
                 var oView = this.getView();
-
                 if (!this._pValueHelpDialogItem) {
                     this._pValueHelpDialogItem = Fragment.load({
                         id: oView.getId(),
@@ -87,35 +97,46 @@ sap.ui.define([
                         oValueHelpDialogItem.open();
                     }.bind(this)
                 );
-
-               
                 },
                 onCancelPressed: function () {
-                    this._pValueHelpDialogItem.then(
-                      function (oDialog) {
-                        oDialog.close();
-                     
-                      }.bind(this)
-                    );
-                    this.byId("Invoice_description_textarea").setValue("");
-                    this.byId("input_amount").setValue("");
+                    if(this._pValueHelpDialogItem){
+                        this._pValueHelpDialogItem.then(
+                            function (oDialog) {
+                              oDialog.close();
+                           
+                            }.bind(this)
+                          );
+                          this.byId("Invoice_description_textarea").setValue("");
+                          this.byId("input_amount").setValue("");
+                    }
+                    else if(this._pValueHelpDialogItemEdit){
+                        this._pValueHelpDialogItemEdit.then(
+                            function (oDialogEdit) {
+                              oDialogEdit.close();
+                           
+                            }.bind(this)
+                          );
+                          this.byId("Invoice_description_textarea").setValue("");
+                          this.byId("input_amount").setValue("");
+                    }
                   },
                   onCreate:function(data){
-                    console.log(data);
                     var oList = this.byId("main_table");
                     var oBinding = oList.getBinding("items");
-                    console.log(oBinding);
+                    // console.log(oBinding);
                     var _data = {
                         id: this.count,
                         description: data.description,
                         amount: data.amount,
                       };
                     var oModel = this.getView().getModel("oItemData").getData();
-                    console.log(oModel)
+                    // console.log(oModel)
                     oModel.Items.push(_data);
                     this.getView().getModel("oItemData").setData(oModel);
-                    console.log(oModel);
+                    // console.log(oModel);
                     this.count++;
+                    this._totalCalculation();
+
                   },
                   onSavePresseditem:function(){
                     var _description = this.byId("Invoice_description_textarea");
@@ -131,23 +152,64 @@ sap.ui.define([
                     this.byId("Invoice_description_textarea").setValue("");
                     this.byId("input_amount").setValue("");
 
-
-                    
                   },
-                  onDeletePresseditem:function(){
-
+                  onDeletePresseditem:function(oEvent){
+                    const oTotalCounterModel = this.getView().getModel("oTotalCounterModel");
+                    var getObjectId =  oEvent.getSource().getBindingContext("oItemData").getObject().id;
+                    var oModel = this.getView().getModel("oItemData").getData();
+                    for (let i = 0; i < oModel.Items.length; i++) {
+                        var temp = oModel.Items[i];
+                        if (temp.id === getObjectId) {
+                          var index = i;
+                          this._total -= temp.amount;
+                          oTotalCounterModel.setProperty("/total", this._total);
+                          temp = "";
+                          break;
+                        }
+                      }
+                      oModel.Items.splice(index, 1);
+                    this.getView().getModel("oItemData").setData(oModel);
+                   
                   },
                   onEditItemPress:function(oEvent){
-                    var  oSource = oEvent.getSource();
-                    var row = oEvent.getSource().findAggregatedObjects();
-                    console.log(row);
-                    // this.byId("Invoice_description_textarea").setValue("shopnil");
-                    // this.onAddItem();
+                    var getObject =  oEvent.getSource().getBindingContext("oItemData").getObject();
+                    var oView = this.getView();
+                    console.log(getObject);
+                    if (!this._pValueHelpDialogItemEdit) {
+                        this._pValueHelpDialogItemEdit = Fragment.load({
+                            id: oView.getId(),
+                            name: "dahill.dahill.view.fragment.CreateItem",
+                            controller: this,
+                        }).then(function (oValueHelpDialogItemEdit) {
+                            oView.addDependent(oValueHelpDialogItemEdit);
+                            return oValueHelpDialogItemEdit;
+                        });
+                    }
+                    this._pValueHelpDialogItemEdit.then(
+                        function (oValueHelpDialog) {
+                          this.byId("Invoice_description_textarea").setValue(getObject.description);
+                          this.byId("input_amount").setValue(getObject.amount);
+                            oValueHelpDialog.open();
+                        }.bind(this)
+                    );
                    
                 },
-                  
-                    
-    
+                _totalCalculation:function(){
+                    const oTotalCounterModel = this.getView().getModel("oTotalCounterModel");
+                    var oModel = this.getView().getModel("oItemData").getData();
+                    let arr = [];
+                    for (let i = 0; i < oModel.Items.length; i++) {
+                        var temp = oModel.Items[i];
+                        arr.push(temp.amount)
+                      }
+                     this._total = 0;
+                     for(var i=0; i < arr.length; i++){
+                        this._total += parseInt(arr[i]);
+                    }
+                    oTotalCounterModel.setProperty("/total", this._total);
+                },
+                
+               
         });
     });
   
